473db661dc7e5706ef0dec5f81ed430ded022637
Adam Cottrill <33773276+adacott@users.noreply.github.com>
2023-09-21 20:06:40 +0000

    Refactored background change code, added rainbow

--- logic.js
+++ logic.js
@@ -1,31 +1,63 @@
-function drawGrid() {
+// Find appropriate dimensions of div container
+const cont = document.querySelector(".div-container");
+const gds = document.querySelector(".grid-size");
+const cw = cont.clientWidth;
+const ch = cont.clientHeight;
+let w = cw / gds.value + "px";
+let h = ch / gds.value + "px";
+let state = "default";
+let color = "#FFFFFF";
+
+function startUp() {
     for (i = 1; i <= (gds.value * gds.value); i++) {
         const nm = document.createElement("div");
         nm.style.border = "1px solid rgba(128, 0, 128, 0.461)"
         nm.classList.add("div-base");
 
-        // The following are for debug purposes to see the grid is correct
-        // const newText = document.createTextNode(`${i}`);
-        // nm.appendChild(newText);
-        // nm.style.color = "White";
-        // 
-
         nm.style.width = `${w}`;
         nm.style.height = `${h}`;
-        // console.log(nm.style.width);
         cont.appendChild(nm);
     }
 }
 
-function changeBackground(e) {
-    if (e.target.className == "div-base") {
-        e.target.classList.add("hover");
+function changeBackground() {
+    switch (state) {
+        case "default":
+            this.style.backgroundColor = "#FFFFFF";
+            break;
+        case "pickColor":
+            this.style.backgroundColor = color;
+            break;
+        case "rainbow":
+            function random(min, max) {
+                return Math.floor(Math.random() * (max - min + 1) + min);
+            }
+            r = random(1, 256);
+            g = random(1, 256);
+            b = random(1, 256);
+
+            this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            break;
+    }
+}
+
+function userColor(e) {
+    state = "pickColor"
+    color = e.target.value;
+}
+
+function rainbowToggle() {
+    if (this.checked) {
+        state = "rainbow";
+    }
+    else if (!this.checked) {
+        state = "pickColor";
     }
 }
 
 function resetBackground() {
-    divs = document.querySelectorAll(".hover");
-    divs.forEach(dv => dv.classList.remove("hover"));
+    divs = document.querySelectorAll(".div-base");
+    divs.forEach(dv => dv.style.backgroundColor = "#000000");
 }
 
 function modifyGrid() {
@@ -42,37 +74,46 @@ function modifyGrid() {
         const nm = document.createElement("div");
         nm.style.border = "1px solid rgba(128, 0, 128, 0.461)";
         nm.classList.add("div-base");
-        nm.style.width = `${w}`;
-        nm.style.height = `${h}`;
+        nm.style.width = `${w} `;
+        nm.style.height = `${h} `;
         cont.appendChild(nm);
     }
 
     const tx = document.querySelector(".grid-label");
-    tx.textContent = `Grid-size: ${gds.value}`;
+    tx.textContent = `Grid - size: ${gds.value} `;
 
     // reinstate new event listeners for newly created divs
     let divs = document.querySelectorAll(".div-base");
     divs.forEach(dv => dv.addEventListener("mouseover", changeBackground));
 }
 
-// Records current container size and auto-calculates and draws grid
-const cont = document.querySelector(".div-container");
-let gds = document.querySelector("input");
-const cw = cont.clientWidth;
-const ch = cont.clientHeight;
-let w = cw / gds.value + "px";
-let h = ch / gds.value + "px";
+// Draw the initial grid
+window.onload = startUp();
 
-
-// For a 16x16 grid, create 256 squares that are scaled based on width and height of the container
-
-window.onload = drawGrid();
-
-// Event listeners for the whole page
-let divs = document.querySelectorAll(".div-base");
-divs.forEach(dv => dv.addEventListener("mouseover", changeBackground));
-
+// Select reset button, the divs to color, and the color picker
 const btn = document.querySelector(".reset");
-btn.addEventListener("click", resetBackground);
+let divs = document.querySelectorAll(".div-base");
+colorPicker = document.querySelector("#color-picker");
+rainbow = document.querySelector("#rainbow");
 
+// Create eventlisteners for all selections above
+divs.forEach(dv => dv.addEventListener("mouseover", changeBackground));
+btn.addEventListener("click", resetBackground);
 gds.addEventListener("change", modifyGrid);
+colorPicker.addEventListener("change", userColor);
+colorPicker.addEventListener("input", userColor);
+rainbow.addEventListener("input", rainbowToggle);
+
+
+
+
+//TODO: 
+// Create a way for changeBackground to access and use the color input.
+// Create a rainbow function that when rainbow is checkmarked it changes "changeBackground" function
+// to randomly select the colors
+
+// May need to refactor my code to change the background color instead of adding and removing
+// a class -- it is just such a MASSIVE pain in the fucking ass to select something thats not
+// in the DOM tree.
+
+
